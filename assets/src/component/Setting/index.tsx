a8d78f8f6af38c765360afb676bbe73e02f2ccf6
Franciscowxp <franciscowxp@gmail.com>
2019-05-10 16:27:47 +0800

    optimize list component and add refresh logic

--- assets/src/component/Setting/index.tsx
+++ assets/src/component/Setting/index.tsx
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import { ipcRenderer } from 'electron';
 import Context from '~src/context';
 
 import { EAction } from '~cModel/action';
@@ -8,17 +9,36 @@ import './style.css';
 import { TFuncVoid } from '~util';
 
 export default React.memo(() => {
-  const { state: { security }, dispatch } = useContext(Context);
-  const handleClick: TFuncVoid = (): void => {
-    console.log('click');
-    dispatch({ type: EAction.setSecurity, payload: !security });
+  const { state: { security, loading, page }, dispatch } = useContext(Context);
+  const handleSecurityClick: TFuncVoid = (): void => {
+    dispatch({
+      type: EAction.setSecurity,
+      payload: !security
+    });
+  };
+
+  const handleRefreshClick: TFuncVoid = (): void => {
+    ipcRenderer.send('image-post', { page, tags: '' });
+    dispatch({
+      type: EAction.setLoading,
+      payload: true
+    });
   };
 
   return (
-    <section className={`setting ${security ? 'active' : ''}`}>
-      <label className='sToggle' onClick={handleClick}>
-        <span className='sFake'/>
-      </label>
+    <section className='setting'>
+      <article className={`sSecurity ${security ? 'active' : ''}`}>
+        <label className='sToggle' onClick={handleSecurityClick}>
+          <span className='sFake'/>
+        </label>
+      </article>
+      <article className={`sRefresh ${loading ? 'active' : ''}`} onClick={handleRefreshClick}>
+        <div />
+        <div />
+        <div />
+        <div />
+        <div />
+      </article>
     </section>
   );
 });
